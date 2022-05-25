20bf746084416f28af5f2462b5bec81a8ca6e490
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-25 13:53:24 -0500

    map to players ,structure and stores

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 
-
-const Players = () => {
+const Players = ({players}) => {
   return (
-    <div>Players</div>
+    <section>
+      <h2>Players</h2>
+      <div className="container-players">
+        {
+          players?.map(p=>(
+              <article className='players'>
+                <img src={p.pic} alt={p.name} />
+                <h3>{p.name}</h3>
+                <div>
+                  <button onClick={() => addHeadline(p)}>Headline</button>
+                  <button>Subtitute</button>
+                </div>
+              </article>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
-export default Players
+const mapStateToProps = state =>({
+    players: state.players
+})
+
+
+const mapDispatchToProps = dispatch => ({})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Players)
