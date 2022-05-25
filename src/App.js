5cbbe757878326d8db488041a52cf8472cb1fdc5
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-25 13:52:27 -0500

    adding provider and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import Players from './components/Players';
 import TeamSelect from './components/TeamSelect';
+import store from './store'
 
 
 
 const App = () => {
   return (
+  <Provider store={store}> 
     <main>
       <h1>Soccer Team Dream!</h1>
       <Players/>
       <TeamSelect/>
     </main>
+  </Provider>
   )
 }
 
