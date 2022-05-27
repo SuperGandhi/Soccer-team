46170340c67af4e659193885ecc00069ac15038e
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-26 23:31:00 -0500

    add map and event onclick

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 
-const Players = ({players}) => {
+const Players = ({players, addHeadline}) => {
   return (
     <section>
       <h2>Players</h2>
       <div className="container-players">
         {
           players?.map(p=>(
-              <article className='players'>
+              <article className='players' key={p.id}>
                 <img src={p.pic} alt={p.name} />
                 <h3>{p.name}</h3>
                 <div>
@@ -28,8 +28,15 @@ const mapStateToProps = state =>({
     players: state.players
 })
 
-
-const mapDispatchToProps = dispatch => ({})
+// functions that are converted by properties
+const mapDispatchToProps = dispatch => ({
+  addHeadline(player){
+    dispatch({
+      type: 'ADD_HEADLINE',
+      player
+    })
+  }
+})
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Players)
