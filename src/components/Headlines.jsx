aeff2ab0fb5180e95886205eca291beba8a0f263
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-28 23:40:14 -0500

    action quit headline

--- src/components/Headlines.jsx
+++ src/components/Headlines.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux/es/exports'
 
-const Headlines = ({headlines}) => {
+const Headlines = ({headlines, quitHeadline}) => {
   return (
     <section>
         <h2>Headlines</h2>
@@ -11,7 +11,7 @@ const Headlines = ({headlines}) => {
                     <article className='headline' key={p.id}>
                         <div>
                             <img src={p.pic} alt={p.name} />
-                            <button>X</button>
+                            <button onClick={()=> quitHeadline(p)}>X</button>
                         </div>
                         <p>{p.name}</p>
                     </article>
@@ -26,7 +26,14 @@ const mapStateToProps = state =>({
     headlines: state.players
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    quitHeadline(player){
+        dispatch({
+            type: 'QUIT_HEADLINE',
+            player
+        })
+    }
+})
 
 
 export default connect (mapStateToProps, mapDispatchToProps)(Headlines)
